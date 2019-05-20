aa0462202bf39891792948b4817ad1efe10c2255
KF <kevin.fagot@edu.itescia.fr>
2019-05-20 11:32:06 +0200

    finish fibonacci & start stairs

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: azz-kevinf
- * Date: 2019-05-20
- * Time: 09:28
- */
+
+
+    function Fibonachi($n){
+
+        $a = 0;
+        $b = 1;
+        $c = 5;
+
+        for ($i = 2; $i <= $n; $i++){
+
+            if($i == 0) {
+                //echo 0 . PHP_EOL;
+            }
+            if($i == 1){
+                //echo 1 . PHP_EOL;
+            }
+            $c = $a + $b;
+            $a = $b;
+            $b = $c;
+
+
+
+        }
+        echo $c."\n\n";
+
+
+    }
+
+    Fibonachi(10);
+Fibonachi(100);
+Fibonachi(1000);
+Fibonachi(10000);
---
+++ stairs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azz-kevinf
+ * Date: 2019-05-20
+ * Time: 10:58
+ */
+
+function stairs($nbMarche, $distancePas){
+
+    $pasTotal = 0;
+
+    for ($i = 0; $i < $nbMarche; $i++){
+
+    }
+
+
+}
